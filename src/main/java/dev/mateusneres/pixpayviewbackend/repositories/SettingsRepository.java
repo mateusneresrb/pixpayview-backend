628be75a912405cec04be1d9baed71370ca366e7
mateusneresrb <mateusneresrb@gmail.com>
2023-04-02 21:17:18 -0300

    feat: some changes in settings routes

--- src/main/java/dev/mateusneres/pixpayviewbackend/repositories/SettingsRepository.java
+++ src/main/java/dev/mateusneres/pixpayviewbackend/repositories/SettingsRepository.java
@@ -3,10 +3,10 @@ package dev.mateusneres.pixpayviewbackend.repositories;
 import dev.mateusneres.pixpayviewbackend.entities.Settings;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SettingsRepository extends JpaRepository<Settings, Long> {
 
-    Settings findFirst();
-
-    String findFirstPaymentToken();
+    Optional<Settings> findFirstByOrderByIdAsc();
 
 }
--- src/main/java/dev/mateusneres/pixpayviewbackend/services/SettingsService.java
+++ src/main/java/dev/mateusneres/pixpayviewbackend/services/SettingsService.java
@@ -1,11 +1,15 @@
 package dev.mateusneres.pixpayviewbackend.services;
 
+import dev.mateusneres.pixpayviewbackend.dtos.response.TokenResponse;
 import dev.mateusneres.pixpayviewbackend.entities.Settings;
+import dev.mateusneres.pixpayviewbackend.exceptions.PaymentTokenNotFoundException;
 import dev.mateusneres.pixpayviewbackend.repositories.SettingsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,8 +17,20 @@ public class SettingsService {
 
     private final SettingsRepository settingsRepository;
 
+    public ResponseEntity<Object> findPaymentToken() {
+        Optional<Settings> optionalSettings = settingsRepository.findFirstByOrderByIdAsc();
+
+        if (optionalSettings.isEmpty()) {
+            throw new PaymentTokenNotFoundException(4001, "The payment token not exists!");
+        }
+
+        String paymentToken = optionalSettings.get().getPaymentToken();
+        
+        return ResponseEntity.ok(new TokenResponse(paymentToken));
+    }
+
     public ResponseEntity<Object> updatePaymentToken(String token) {
-        Settings settings = settingsRepository.findFirst();
+        Settings settings = settingsRepository.findFirstByOrderByIdAsc().orElseGet(Settings::new);
         settings.setPaymentToken(token);
 
         settingsRepository.save(settings);
