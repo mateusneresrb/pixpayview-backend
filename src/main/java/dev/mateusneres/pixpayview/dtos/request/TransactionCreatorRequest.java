3abbd44cabb29f3ea6baa0989a493669d2353c99
mateusneresrb <mateusneresrb@gmail.com>
2023-05-08 23:59:53 -0300

    feat: add setter in TransactionCreatorRequest

--- src/main/java/dev/mateusneres/pixpayview/dtos/request/TransactionCreatorRequest.java
+++ src/main/java/dev/mateusneres/pixpayview/dtos/request/TransactionCreatorRequest.java
@@ -2,7 +2,9 @@ package dev.mateusneres.pixpayview.dtos.request;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
 public class TransactionCreatorRequest {
 
